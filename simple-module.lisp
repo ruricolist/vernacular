131e5b232e37f1cf05f3018567a2265fb2f7ac25
Paul M. Rodriguez <pmr@ruricolist.com>
2018-08-20 14:07:51 -0500

    Bug fix: package dependency.

--- simple-module.lisp
+++ simple-module.lisp
@@ -1,5 +1,6 @@
 (uiop:define-package :vernacular/simple-module
-    (:use-reexport :overlord/simple-module))
+    (:use-reexport :overlord/simple-module)
+  (:import-from :vernacular/shadows))
 
 (defpackage :vernacular/simple-module-user
   (:use :vernacular/simple-module :vernacular/shadows))
