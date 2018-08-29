ecaddf149a2415223f923eadfc032e6504706ec4
Paul M. Rodriguez <pmr@ruricolist.com>
2018-08-29 16:50:52 -0500

    Add hash-table-module.

--- hash-table-module.lisp
+++ hash-table-module.lisp
@@ -1,2 +1,18 @@
-(uiop:define-package :vernacular/hash-table-module
-    (:use-reexport :overlord/hash-table-module))
+(defpackage :vernacular/hash-table-module
+  (:use :cl :alexandria :serapeum)
+  (:import-from :overlord/types :error*)
+  (:import-from :vernacular/module :module-ref :module-exports)
+  (:export :hash-table-module))
+(in-package :vernacular/hash-table-module)
+
+(defun hash-table-module (hash-table)
+  (lret ((module (copy-hash-table hash-table :test #'eq)))
+    (unless (= (hash-table-count module)
+               (hash-table-count hash-table))
+      (error* "Hash table ~a contains duplicate keys."))))
+
+(defmethod module-ref ((m hash-table) name)
+  (gethash name m))
+
+(defmethod module-exports ((m hash-table))
+  (hash-table-keys m))
