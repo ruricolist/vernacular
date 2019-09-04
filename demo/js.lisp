70fec5f8696464d9bfa5ac9a1442e65df56420e8
Paul M. Rodriguez <pmr@ruricolist.com>
2019-09-04 16:47:57 -0500

    Start adding comments to js lang implementation.

--- demo/js.lisp
+++ demo/js.lisp
@@ -1,4 +1,5 @@
 (defpackage :vernacular/demo/js
+  (:documentation "Wrap CL-JS for use as a Vernacular language, for instructional purposes.")
   (:use :cl :alexandria :serapeum :cl-js)
   (:nicknames :demo/js)
   (:import-from :overlord/base
@@ -16,11 +17,19 @@
 
 (in-package :vernacular/demo/js)
 
-;;; XXX The version in cl-js actually inlines the environment in the
-;;; expansion, so without this we would get invalid memory access
-;;; errors every time `require' was called.
+;;; First thing first -- we need to get an ugly hack out of the way.
+;;; CL-JS actually inlines the environment in the expansion, which
+;;; won't work (invalid memory access) when compiling to a FASL. So we
+;;; redefine the function that generates the code for global lookups.
+;;; We can use `load-time-value' in the expansion to keep it
+;;; efficient.
 (defun cl-js::expand-global-lookup (prop)
   `(cl-js::gcache-lookup (load-time-value (cons nil (cl-js::make-cache (cl-js::intern-prop ,prop)))) *env*))
+
+;;; This is the global context we will inject into CL-JS to enable
+;;; modules to require other modules. CL-JS doesn't support the ES6
+;;; static import syntax, so for our purposes a module, from the
+;;; perspective of another module, is just an object.
 
 (defparameter *vernacular-lib*
   (lret ((lib (empty-lib "Vernacular")))
@@ -33,21 +42,33 @@
 (defstruct-read-only
     (js-module
      (:constructor make-js-module
-         (obj &aux (exports-table (object-exports-table obj))
+         (obj &aux (exports-table (module-object-exports-table obj))
                    (exports-list (hash-table-keys exports-table)))))
+  "Wrap as JS module for import-export."
   (obj :type js-obj)
   (exports-table :type hash-table)
   (exports-list :type list))
 
+(defmethod module-exports ((m js-module))
+  (js-module-exports-list m))
+
+(defmethod module-ref ((m js-module) key)
+  (gethash key (js-module-exports-table m)))
+
 (defun object-keys (object)
+  "List the keys of a JavaScript object."
   (collecting
     (js-for-in object #'collect :shallow)))
 
+;;; The calling conventions for JavaScript functions and Lisp
+;;; functions are different.
+
 (defun js-fun->lisp-fun (fun)
+  "Wrap FUN, a JS function, for calling from Lisp."
   (lambda (&rest args)
     (apply #'cl-js::js-funcall fun args)))
 
-(defun object-exports-table (object)
+(defun module-object-exports-table (object)
   (alist-hash-table
    (mapcar (lambda (key)
              (let* ((value (js-prop object key))
@@ -57,12 +78,6 @@
                (cons key fun)))
            (object-keys object))
    :test 'eq))
-
-(defmethod module-exports ((m js-module))
-  (js-module-exports-list m))
-
-(defmethod module-ref ((m js-module) key)
-  (gethash key (js-module-exports-table m)))
 
 (defun wrap-as-module (ast)
   `(:function nil ("exports") (,@(second ast) (:return (:name "exports")))))
