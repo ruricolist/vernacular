1c8a1b5c3552f531ecd062cb6023cdd4a1c3769b
Paul M. Rodriguez <pmr@ruricolist.com>
2020-01-24 15:00:58 -0600

    Rename package.

--- well-known.lisp
+++ well-known.lisp
@@ -1,8 +1,8 @@
-(defpackage :vernacular/well-known-exports
+(defpackage :vernacular/well-known
   (:documentation "Symbols from this package are used in a keyword-like manner.")
   (:use)
   (:export :default :main))
 
 #+sb-package-locks
 (cl:eval-when (:compile-toplevel :load-toplevel :execute)
-  (sb-ext:lock-package (find-package :vernacular/well-known-exports)))
+  (sb-ext:lock-package (find-package :vernacular/well-known)))
